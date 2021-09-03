b1a0e3ba431e8c7b65747bdff3984f85e12adffd
MuhammadSamarah <muhammad.samarah@bryanuniversity.edu>
2021-09-03 09:01:03 -0500

    update

---
+++ Week1/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import ToDo from './TodoList';
+
+function App() {
+  return (
+    <ToDo/>
+
+  );
+}
+
+export default App;
---
+++ Week1/TodoList.js
@@ -0,0 +1,20 @@
+
+function ToDo() {
+
+
+    return (
+        <>
+        <h1>ToDo List</h1>
+        <br/>
+        <h2>Which is better?</h2>
+        <ul>
+            <li><input type = "checkbox"/>Football</li>
+            <li><input type = "checkbox"/>Tennis</li>
+            <li><input type = "checkbox"/>Golf</li>
+            <li><input type = "checkbox"/>Soccer</li>
+        </ul>
+        </>
+    );
+}
+
+export default ToDo;
