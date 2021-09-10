c9ef291971813d14c993ca2599bf86064e027a0b
MuhammadSamarah <muhammad.samarah@bryanuniversity.edu>
2021-09-09 20:23:33 -0500

    update

---
+++ Week2/App.js
@@ -0,0 +1,20 @@
+
+import Card from './component/card'
+
+function App() {
+  return (
+    <div className="container">
+
+    <Card title="Title 1" subtitle="SubTitle 1" description="Hello World" color="red"/>
+
+    <Card title="Title 2" subtitle="SubTitle 2" description="Hello World" color="orange"/>
+
+    <Card title="Title 3" subtitle="SubTitle 3" description="Hello World" color="yellow"/>
+
+    <Card title="Title 4" subtitle="SubTitle 4" description="Hello World" color="green"/>
+    
+    </div>
+  );
+}
+
+export default App;
