f6fdd43d68745be286969ba6b5ea32a01fdbf257
MuhammadSamarah <muhammad.samarah@bryanuniversity.edu>
2021-09-17 13:50:13 -0500

    update

---
+++ Week3ToDoList/src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import ToDo from './ToDoList';
+import { items } from './store';
+import React from "react";
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {items: items}
+
+    this.dltBtn = this.dltBtn.bind(this);
+}
+
+toggleComplete = (e) => {
+    console.log("this checkbox was checked", e.target)
+    var newArray = this.state.items.map(item => {
+      if(e.target.id === item.id){
+        if(item.isCompleted === true){
+          item.isCompleted = false
+          return item
+        } else 
+        {
+          item.isCompleted = true
+
+          return item
+        }
+
+        } else {
+        return item
+    }
+}) 
+    console.log(newArray)
+    this.setState({
+      items: newArray
+    })
+  }
+dltBtn = (e) => {
+    console.log("this item was removed")
+    if(e){
+      let dltd = [...this.state.items];
+      let index = dltd.findIndex(item => item.id === e);
+      dltd.splice(index, 1)
+      this.setState({
+        items: dltd
+      })
+    }
+}
+
+render(){
+    return (
+      <div>
+        <h1>checklist</h1>
+        <ToDo items = {this.state.items} toggleComplete = {this.toggleComplete} dltBtn = {this.dltBtn}/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ Week3ToDoList/src/store.js
@@ -0,0 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const items = [
+    {id: uuidv4(),text: "HomeWork",isCompleted: false},
+
+    {id: uuidv4(),text: "Basketball",isCompleted: false},
+
+    {id: uuidv4(),text: "Lunch",isCompleted: false},
+
+    {id: uuidv4(),text: "Swim",isCompleted: false},
+
+    {id: uuidv4(),text: "Dinner shopping",isCompleted: false}
+]
