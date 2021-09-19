d4a3e8fa9bd69e95ab3790d4b4246f7bb8571fd1
MuhammadSamarah <muhammad.samarah@bryanuniversity.edu>
2021-09-18 20:32:00 -0500

    update

--- Week3ToDoList/src/App.js
+++ Week3ToDoList/src/App.js
@@ -1,59 +1,10 @@
-import './App.css';
-import ToDo from './ToDoList';
-import { items } from './store';
-import React from "react";
-class App extends React.Component {
-  constructor(props){
-    super(props);
+import TodoList from './components/todolist'
+import Data from './store'
 
-    this.state = {items: items}
-
-    this.dltBtn = this.dltBtn.bind(this);
-}
-
-toggleComplete = (e) => {
-    console.log("this checkbox was checked", e.target)
-    var newArray = this.state.items.map(item => {
-      if(e.target.id === item.id){
-        if(item.isCompleted === true){
-          item.isCompleted = false
-          return item
-        } else 
-        {
-          item.isCompleted = true
-
-          return item
-        }
-
-        } else {
-        return item
-    }
-}) 
-    console.log(newArray)
-    this.setState({
-      items: newArray
-    })
-  }
-dltBtn = (e) => {
-    console.log("this item was removed")
-    if(e){
-      let dltd = [...this.state.items];
-      let index = dltd.findIndex(item => item.id === e);
-      dltd.splice(index, 1)
-      this.setState({
-        items: dltd
-      })
-    }
-}
-
-render(){
-    return (
-      <div>
-        <h1>ToDoList 3</h1>
-        <ToDo items = {this.state.items} toggleComplete = {this.toggleComplete} dltBtn = {this.dltBtn}/>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <TodoList data={Data}/>
+  )
 }
 
 export default App;
--- Week3ToDoList/src/store.js
+++ Week3ToDoList/src/store.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export const items = [
+const todos = [
     {id: uuidv4(),text: "HomeWork",isCompleted: false},
 
     {id: uuidv4(),text: "Basketball",isCompleted: false},
@@ -10,4 +10,6 @@ export const items = [
     {id: uuidv4(),text: "Swim",isCompleted: false},
 
     {id: uuidv4(),text: "Dinner shopping",isCompleted: false}
-]
+];
+
+export default todos;
