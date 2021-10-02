565ff6e9da386ee07e3beffc36348151f22ad953
MuhammadSamarah <muhammad.samarah@bryanuniversity.edu>
2021-10-02 15:35:01 -0500

    update

---
+++ Week5Giphy/src/GiphyLookup.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+
+function GiphyLookup({initialQuery}) {
+    const[loading, setLoading] = useState(false);
+    const[error, setError] = useState(false);
+    const[imgSrc, setImageSrc] = useState('');
+    const[alt, setAlt] = useState('');
+    const fetchImage = (query = initialQuery ) =>{
+        setLoading(true);
+        const encodedQuery = encodeURIComponent(query);
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=T58XClYzKSh62rSAb4ThST0s1GlcVNkQ&tag=${encodedQuery}`;
+
+        fetch(url, {
+            method:'GET',
+        }).then(function(response) {
+            if (response.ok) {
+                return response.json();
+            }
+                return Promise.reject(response);
+        }).then(function({data}){
+            setLoading(false);
+            setImageSrc(data.fixed_height_downsampled_url);
+            setAlt(data.title)
+        }).catch(function(error){
+            setLoading(false);
+            setError(true);
+            console.warn('something went wrong',error);
+        });
+    }
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        fetchImage(evt.target.query.value)
+    }
+    useEffect(fetchImage, [initialQuery]);
+
+    return (
+        <div>
+            <br></br>
+        <header>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="query" placeholder="search phrase" style={{width:"225px"}}/> &nbsp;
+                <button type="submit">enter</button>
+            </form>
+        </header>
+            <br></br>
+        <main>
+            <section>
+                {loading &&<div>Loading...</div>}
+                {error &&<div>Error</div>}
+                {!loading && !error && !imgSrc &&
+                <div>Nothing Found</div>}
+                {!loading && !error && imgSrc &&
+                <div><img src={imgSrc} alt = {alt} style={{width:'300px'}}/></div>
+                }
+
+            </section>
+        </main>
+        </div>
+    )
+ }                                    
+export default GiphyLookup
